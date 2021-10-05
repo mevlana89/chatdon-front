948648262e7502f75c24f472139cca35656deeeb
levequesa <79331585+levequesa@users.noreply.github.com>
2021-10-05 08:53:06 +0200

    creation

--- src/app/utilisateur/donateur/donateur.component.ts
+++ src/app/utilisateur/donateur/donateur.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { Donateur } from '../donateur';
+import { UtilisateurService } from '../utilisateur.service';
+
 
 @Component({
   selector: 'app-donateur',
@@ -7,9 +13,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonateurComponent implements OnInit {
 
-  constructor() { }
+  hide = true;
+  private baseUrl = environment.wsRootUrl;
+  newDonateur = new Donateur;
+
+  constructor(private fb: FormBuilder, public utilisateurService: UtilisateurService, public router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  createDonateurForm = this.fb.group ({
+    id:[''],
+    nom: [''],
+    prenom: [''],
+    mail: [''],
+    telephone: [''],
+    motDePasse1: [''],
+    motDePasse2: [''],
+    rue:[''],
+    codePostal:[''],
+    ville:[''],
+    chatsProposes:['']
+  })
+
+  addDonateur(){
+    this.utilisateurService.createDonateur(this.newDonateur)
+         .subscribe( (data:Donateur) => console.log(data));
+    //                                  this.router.navigate(['/donateurs',donateur.id]));
   }
 
 }
