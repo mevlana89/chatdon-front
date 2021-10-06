0d54e3dab252b7cfbf59ddf18eaff318f7e806fe
levequesa <79331585+levequesa@users.noreply.github.com>
2021-10-06 07:11:04 +0200

    ajout app-candidat dans donateur

--- src/app/utilisateur/donateur/donateur.component.ts
+++ src/app/utilisateur/donateur/donateur.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { Donateur } from '../donateur';
@@ -15,11 +15,14 @@ export class DonateurComponent implements OnInit {
 
   hide = true;
   private baseUrl = environment.wsRootUrl;
-  newDonateur = new Donateur;
+  readonly radioFormControl = new FormControl(null);
+
+ // donateur :Donateur = new Donateur();
 
   constructor(private fb: FormBuilder, public utilisateurService: UtilisateurService, public router: Router) { }
 
   ngOnInit(): void {
+
   }
 
   createDonateurForm = this.fb.group ({
@@ -30,16 +33,31 @@ export class DonateurComponent implements OnInit {
     telephone: [''],
     motDePasse1: [''],
     motDePasse2: [''],
-    rue:[''],
-    codePostal:[''],
-    ville:[''],
-    chatsProposes:['']
+
+      rue:[''],
+      codePostal:[''],
+      ville:[''],
+
+    //chatsProposes:['[]']
   })
 
+  addProfil(){
+    if (this.createDonateurForm.valid){
+      if (this.radioFormControl.value == 1) {
+         this.addDonateur();
+        }
+      else {
+        console.log("canditat");
+      }
+    }
+  }
   addDonateur(){
-    this.utilisateurService.createDonateur(this.newDonateur)
-         .subscribe( (data:Donateur) => console.log(data));
-    //                                  this.router.navigate(['/donateurs',donateur.id]));
+    if (this.createDonateurForm.valid) {
+      const donateur:Donateur = this.createDonateurForm.value;
+      this.utilisateurService.createDonateur(donateur)
+          .subscribe( (data:Donateur) => { console.log(data) } );
+    }
+      //   this.router.navigate(['/donateurs',donateur.id]));
   }
 
 }
