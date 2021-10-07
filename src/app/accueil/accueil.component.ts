00a386ff6428d3a9cc909323f4826644f84a18ac
ergunmu1 <mustafa.ergun@natixis.com>
2021-10-07 15:12:09 +0200

    ajout du filtre (recherche) sur la recherche des chats

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FilterDto } from '../shared/liste-chat-light/FilterDto';
+import { TAILLES, SEXES, RACES, CATEGORIES_AGE, PELAGES, CARACTERES, REGIONS } from '../shared/listes';
+import { AbstractControl, FormArray, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-accueil',
@@ -7,9 +10,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccueilComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+
+  public displaySearch = false;
+
+    // Declare all controls with validation rules
+    orderForm = this.fb.group({
+      categorieAge: ['',/*Validators.required*/],
+      race: ['', [/*Validators.required*/]],
+      sexe: ['', [/*Validators.required*/]],
+      taille: ['', [/*Validators.required*/]],
+      pelage: ['', [/*Validators.required*/]],
+      caractere: ['', [/*Validators.required*/]],
+      zoneGeo: ['', [/*Validators.required*/]],
+      sociableChat: ['', [/*Validators.required*/]],
+      sociableChien: ['', [/*Validators.required*/]],
+      sociableEnfant: ['', [/*Validators.required*/]]
+    });
+
+  filterDto: FilterDto = new FilterDto();
+
+  public Tailles = TAILLES;
+
+  public Sexes = SEXES;
+
+  public Races = RACES;
+
+  public Cat_Age = CATEGORIES_AGE;
+
+  public Pelages = PELAGES;
+
+  public Caracteres = CARACTERES;
+
+  public Regions = REGIONS;
 
   ngOnInit(): void {
+/**
+        // get Observable from FormGroup
+        this.orderForm.valueChanges
+        // listen to value change
+        .subscribe(value => {
+          console.log('orderForm value changes : ', value);
+        });
+         */
+        this.filterDto = this.orderForm.value;
+        //console.log("this.filterDto.race: ", this.filterDto.race);
+
   }
 
+  rechercheChats() {
+    this.filterDto = this.orderForm.value;
+
+    //console.log('oderForm submitted : ', this.orderForm.value);
+    //this.ngOnInit();
+
+  }
+  searchDisplay(){
+    this.displaySearch = !this.displaySearch;
+  }
 }
--- src/app/shared/liste-chat-light/liste-chat-light.service.ts
+++ src/app/shared/liste-chat-light/liste-chat-light.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http'
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 import { FilterDto } from './FilterDto';
@@ -20,7 +20,70 @@ export class ListeChatLightService {
 
   constructor(private http: HttpClient) {
    }
-   getAllUnreservedCats(filterDTO : FilterDto): Observable<any>{
-    return this.http.get<any>(`${this.baseUrl}` + "/chats/", this.optionRequete);
+   
+   getAllUnreservedCats(filterDTO : FilterDto): Observable<any>
+   {
+    let parametres = new HttpParams();
+    
+    if (filterDTO.categorieAge != undefined)
+    {
+      parametres = parametres.append('categorieAge', filterDTO.categorieAge);
+    }
+    if (filterDTO.race != undefined)
+    {
+      parametres = parametres.append('race', filterDTO.race);
+    }
+    if (filterDTO.sexe != undefined)
+    {
+      parametres = parametres.append('sexe', filterDTO.sexe);
+    }
+    if (filterDTO.taille != undefined)
+    {
+      parametres = parametres.append('taille', filterDTO.taille);
+    }
+    if (filterDTO.pelage != undefined)
+    {
+      parametres = parametres.append('pelage', filterDTO.pelage);
+    }
+    if (filterDTO.caractere != undefined)
+    {
+      parametres = parametres.append('caractere', filterDTO.caractere);
+    }
+    if (filterDTO.zoneGeo != undefined)
+    {
+      parametres = parametres.append('zoneGeo', filterDTO.zoneGeo);
+    }
+
+    if (filterDTO.sociableChat != undefined)
+    {
+      parametres = parametres.append('sociableChat', filterDTO.sociableChat);
+    }
+
+    if (filterDTO.sociableChien != undefined)
+    {
+      parametres = parametres.append('sociableChien', filterDTO.sociableChien);
+    }
+
+    if (filterDTO.sociableEnfant != undefined)
+    {
+      parametres = parametres.append('sociableEnfant', filterDTO.sociableEnfant);
+    }
+
+    // if (filterDTO.sociableChat == true)
+
+
+    // let parametres2 = new HttpParams({
+    //   fromObject: filterDTO
+    // });
+
+//      let httpParams = new HttpParams();
+//  Object.keys(filterDTO).forEach(function (key) {
+//       httpParams = httpParams.append(key, filterDTO[key]);
+//  });
+
+
+//parametres = parametres.append('filterDTO', filterDTO.zoneGeo );
+
+    return this.http.get<any>(`${this.baseUrl}` + "/chats", { params: parametres } );
   }
 }
